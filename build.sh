a92094061468259dbdfa6554d85fdf8343901da1
totravel <totravel@foxmail.com>
2022-07-20 17:32:45 +0800

    update build system

--- build.sh
+++ build.sh
@@ -8,18 +8,23 @@ else
 fi
 parent_dir=$(dirname $script_dir)
 
+if [ "$OSTYPE" == "msys" ]; then
+  build_dir=$script_dir/build-win
+else
+  build_dir=$script_dir/build-linux
+fi
+
 build_type=Release
 build_shared_libs=ON
-arch=x64 # or Win32
+# Windows
+host=x64 # x64 or x86
+arch=x64 # x64 or Win32
+# Linux
+build_x86=OFF
 
-install_dir=install
 zip_dir=$parent_dir/3rd_party/zip-0.2.1
 pugixml_dir=$parent_dir/3rd_party/pugixml-1.12.1
 minidocx_dir=$script_dir
-
-mkdir -p build
-cd build
-# rm -rf *
 
 options="
   -DBUILD_SHARED_LIBS=$build_shared_libs
@@ -27,12 +32,16 @@ options="
   -DPUGIXML_DIR=$pugixml_dir
   -DMINIDOCX_DIR=$minidocx_dir"
 
+echo -e "\e[36mBuilding...\e[0m"
+
+if [ ! -d "$build_dir" ]; then
+  mkdir $build_dir
+fi
+
 if [ "$OSTYPE" == "msys" ]; then
-  cmake .. $options -A $arch
-  cmake --build . --config $build_type -- -m:4
-  cmake --install . --prefix $install_dir --config $build_type
+  cmake -S $script_dir -B $build_dir -Thost=$host -A $arch $options
+  cmake --build $build_dir --config $build_type -- -m:4
 else
-  cmake .. $options -DCMAKE_BUILD_TYPE=$build_type
-  cmake --build . -- -j4
-  cmake --install . --prefix $install_dir
+  cmake -S $script_dir -B $build_dir -DCMAKE_BUILD_TYPE=$build_type -DBUILD_X86=$build_x86 $options
+  cmake --build $build_dir -- -j4
 fi
