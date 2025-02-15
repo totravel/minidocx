978029828e46b6b8e14e0da52a90166efc5af6f9
totravel <totravel@foxmail.com>
2025-02-15 20:11:57 +0800

    Add license

--- include/minidocx/packaging/relationship.hpp
+++ include/minidocx/packaging/relationship.hpp
@@ -1,4 +1,9 @@
-﻿
+﻿/**
+ * Copyright (C) 2022-2025, Xie Zequn <totravel@foxmail.com>. All rights reserved.
+ * Distributed under the MIT License (http://opensource.org/licenses/MIT)
+ * Report bugs and download new versions at https://github.com/totravel/minidocx
+ */
+
 #pragma once
 
 #include "packaging/part.hpp"
--- include/minidocx/utils/zip.hpp
+++ include/minidocx/utils/zip.hpp
@@ -1,4 +1,9 @@
-﻿
+﻿/**
+ * Copyright (C) 2024-2025, Xie Zequn <totravel@foxmail.com>. All rights reserved.
+ * Distributed under the MIT License (http://opensource.org/licenses/MIT)
+ * Report bugs and download new versions at https://github.com/totravel/zip
+ */
+
 #pragma once
 
 #include <iosfwd>
